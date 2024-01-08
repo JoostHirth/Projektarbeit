1e77aea5d50ff90c58144d8c369188920d35c861
Your Name <you@example.com>
2024-01-08 12:22:50 +0100

    2

--- quiz.php
+++ quiz.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
-$username = "root";
-$password = "";
+$username = "Joost";
+$password = "12345";
 $dbname = "quiz";
 
 $con = new mysqli($servername, $username, $password, $dbname);
