920370d83e3a12bfd7966114e083f356865a61eb
Mare816 <markovincetic4@gmail.com>
2024-02-22 15:36:21 +0100

    xy

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const container = document.getElementById('container');
-const registerBtn = document.getElementById('register');
-const loginBtn = document.getElementById('login');
+const registerBtn = document.getElementById('registerBtn');
+const loginBtn = document.getElementById('loginBtn');
 
 registerBtn.addEventListener('click', () => {
     container.classList.add("active");
