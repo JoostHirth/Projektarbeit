3bfee62325254d3bfb28a245d1036f02d372851a
JoostHirth <joost.hirth@outlook.com>
2024-03-26 18:25:50 +0100

    8

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
-const container = document.getElementById('container');
-const registerBtn = document.getElementById('registerBtn');
-const loginBtn = document.getElementById('loginBtn');
+document.addEventListener('DOMContentLoaded', () => {
+    const container = document.querySelector('.container');
+    const registerBtn = document.getElementById('registerBtn');
+    const loginBtn = document.getElementById('loginBtn');
+    const gastBtn = document.querySelector('.gast-form .btn'); // Gast-Button auswählen
 
-registerBtn.addEventListener('click', () => {
-    container.classList.add("active");
-});
+    registerBtn.addEventListener('click', () => {
+        container.classList.add("active");
+    });
 
-loginBtn.addEventListener('click', () => {
-    container.classList.remove("active");
+    loginBtn.addEventListener('click', () => {
+        container.classList.remove("active");
+    });
+
+    gastBtn.addEventListener('click', () => {
+        <a href="hauptseite.php">Spiel starten</a>
+    });
 });
