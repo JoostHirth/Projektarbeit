4a18206345374c5aae5a82199a6fcb4f8c14fe4b
Your Name <you@example.com>
2024-04-16 18:28:00 +0200

    xfvbnml,

--- quiz.php
+++ quiz.php
@@ -97,8 +97,9 @@ if ($id > $vorherige_id || $id == 1) {
     
     $frage = $row_fragetext['frage_text'];
     
-    $sql_antworttext1 = "SELECT antwort_text FROM $antwort_thema WHERE frage_id = '$id' AND antwort_id = '1'";
+    $sql_antworttext1 = "SELECT antwort_text FROM $antwort_thema WHERE frage_id = $id AND antwort_id = '1'";
     $result_antworttext1 = mysqli_query($conn, $sql_antworttext1);
+    print_r($result_antworttext1);
     $row_antworttext1 = mysqli_fetch_assoc($result_antworttext1);
     
     $antwort1 = $row_antworttext1['antwort_text'];
